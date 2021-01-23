8832a7bf499f99c59b6cf3a56776d8626fb2212e
Kevin Hsu <kevin.hsukaihao@gmail.com>
2021-01-23 12:53:53 -0800

    Add Complete order

--- functions/index.js
+++ functions/index.js
@@ -40,7 +40,7 @@ exports.getOrders = functions.https.onCall( async (data, context) => {
   // fetch order data
   storeId = data.storeId;
 
-  const db = admin.firestore()
+  const db = admin.firestore();
   const ordersRef = await db.collection('order');
 
   var orders = [];
@@ -68,6 +68,24 @@ exports.getOrders = functions.https.onCall( async (data, context) => {
   }
 
   //console.log(returnData)
+
+  return returnData;
+});
+
+exports.completeOrder = functions.https.onCall( async (data, context) => {
+  // fetch order data
+  orderId = data.orderId;
+
+  const db = admin.firestore();
+  const orderRef = await db.collection('order').doc(orderId);
+
+  return orderRef.update({
+    completed: true
+  }).then(function(){
+    console.log("order complete successful");
+  }).catch(function(error) {
+    console.log("order complete error: ", error);
+  });
 
   return returnData;
 });
