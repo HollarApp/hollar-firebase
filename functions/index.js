47fd8bffb1d070cc7ea66ed2c67feb7571829fa6
Kevin Hsu <kevin.hsukaihao@gmail.com>
2021-01-23 22:08:52 -0800

    Fix for getOrder

--- functions/index.js
+++ functions/index.js
@@ -40,41 +40,37 @@ exports.getOrders = functions.https.onCall( async (data, context) => {
   // fetch order data
   storeId = data.storeId;
 
-  console.log("storeID: " + storeId);
+  console.info("-------storeID: " + storeId);
+
+  const db = admin.firestore();
+  const querySnapshot = await db.collection('order').where("storeID", "==", storeId).get();
+
   var orders = [];
   var completedOrders = [];
   var pendingOrders = [];
 
-  const db = admin.firestore();
-  const ordersRef = db.collection('order').where("storeID", "==", storeId)
-    .get()
-    .then(function(querySnapshot) {
-      querySnapshot.forEach(function(doc){
-        console.log(JSON.stringify(doc));
-        var tempOrder = {...doc.data()};
-        tempOrder["orderID"] = doc.id;
-        orders.push(tempOrder);
-        console.log("orders: ", orders);
-      });
-      orders.forEach((order, index) => {
-        orders.completed ? completedOrders.push(order) : pendingOrders.push(order);
-      });
+  querySnapshot.forEach(function(doc){
+    console.info("-----doc: " + JSON.stringify(doc));
+    var tempOrder = {...doc.data()};
+    tempOrder["orderID"] = doc.id;
+    console.info("------doc.id: " + doc.id);
 
-      const returnData = {
-        "completedOrders": completedOrders, 
-        "pendingOrders": pendingOrders, 
-        "orders": orders
-      }
-      return returnData;
-    })
-    .catch(function(error) {
-      console.log("Error in getOrders: ", error);
-      return returnData = {
-        "completedOrders": [], 
-        "pendingOrders": [], 
-        "orders": "error"
-      }
-    });
+    orders.push(tempOrder);
+    console.info("-----orders: ", orders);
+  });
+  console.info("-----collected all matching order");
+  orders.forEach((order, index) => {
+    order.completed ? completedOrders.push(order) : pendingOrders.push(order);
+  });
+  console.info("---completed length: " + completedOrders.length);
+  console.info("---pending length: " + pendingOrders.length);
+
+  const returnData = {
+    "completedOrders": completedOrders, 
+    "pendingOrders": pendingOrders, 
+    "orders": orders
+  };
+  return returnData;
 
 });
 
